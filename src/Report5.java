2e995cae9be500dae01161c29088734fa00840e0
e205734 <e205734@ie.u-ryukyu.ac.jp>
2020-12-20 13:55:31 +0900

    ステップ２終了

--- src/Report5.java
+++ src/Report5.java
@@ -1,6 +1,12 @@
 public class Report5 {
+
     public static void main(String[] args) throws Exception {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptioが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
