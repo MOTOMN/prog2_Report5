18ef804425defaa1c4e1bd0463d5f1549cef9b49
e205734 <e205734@ie.u-ryukyu.ac.jp>
2020-12-20 14:16:39 +0900

    ステップ3終了

--- src/Report5.java
+++ src/Report5.java
@@ -1,12 +1,18 @@
 public class Report5 {
 
     public static void main(String[] args) throws Exception {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerExceptioが発生しました。");
-            System.out.println(e.getMessage());
-        }
+        //（ステップ２）
+        // try {
+        //     String str = null;
+        //     System.out.println(str.length());
+        // } catch (NullPointerException e) {
+        //     System.out.println("NullPointerExceptioが発生しました。");
+        //     System.out.println(e.getMessage());
+        // }
+
+        //（ステップ３）
+        String str = "百二十三";
+        int value =Integer.parseInt(str);
+
     }
 }
